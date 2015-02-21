d7a26e3d541f17c933bc910e0d57fb5886489be4
Scott Hanson <scooter.seh@gmail.com>
2015-02-21 13:10:09 -0500

    Moved to DallasTemperature library

--- MQTTGarage/MQTTGarage.ino
+++ MQTTGarage/MQTTGarage.ino
@@ -2,6 +2,7 @@
 #include <Ethernet.h>
 #include <PubSubClient.h>
 #include <OneWire.h>
+#include <DallasTemperature.h>
 
 #define MQTT_CLIENTID "garageArduino"
 //#define MQTT_SERVER "192.168.5.148"
@@ -44,6 +45,8 @@ int prevPhotoValue = 0;
 float prevTemperatureValue = 150;
 
 OneWire  ds(3);// on pin 3 (a 4.7K resistor is necessary)
+DallasTemperature sensors(&ds);
+DeviceAddress outsideThermometer;
 
 EthernetClient ethClient;
 PubSubClient client(server, 1883, callback, ethClient);
@@ -72,6 +75,10 @@ void setup()
     client.publish(willtopic, ipstr);
     digitalWrite(StatLED, HIGH);
   }
+  sensors.begin();
+  sensors.getAddress(outsideThermometer, 0);
+  sensors.setResolution(outsideThermometer,9);
+  
 }
 
 void loop()
@@ -308,99 +315,14 @@ int ReadAnalogValue(int pinNumber)
 }
 
 float ReadTemperature(){
- byte i;
-  byte present = 0;
-  byte type_s;
-  byte data[12];
-  byte addr[8];
-  float celsius, fahrenheit;
+
+  //celsius = (float)raw / 16.0;
+  //fahrenheit = celsius * 1.8 + 32.0;
   
-  if ( !ds.search(addr)) 
-  {
-    //Serial.println("No more addresses.");
-    //Serial.println();
-    ds.reset_search();
-    delay(250);
-    return 0;
-  }
-  
-  //Serial.print("ROM =");
-  //for( i = 0; i < 8; i++) {
-  //  Serial.write(' ');
-  //  Serial.print(addr[i], HEX);
-  //}
+   sensors.requestTemperatures();
 
-  if (OneWire::crc8(addr, 7) != addr[7]) {
-      Serial.println("CRC is not valid!");
-      return 0;
-  }
-  //Serial.println();
- 
-  // the first ROM byte indicates which chip
-  switch (addr[0]) {
-    case 0x10:
-      //Serial.println("  Chip = DS18S20");  // or old DS1820
-      type_s = 1;
-      break;
-    case 0x28:
-      //Serial.println("  Chip = DS18B20");
-      type_s = 0;
-      break;
-    case 0x22:
-      //Serial.println("  Chip = DS1822");
-      type_s = 0;
-      break;
-    default:
-      Serial.println("Device is not a DS18x20 family device.");
-      return 0;
-  } 
-
-  ds.reset();
-  ds.select(addr);
-  ds.write(0x44, 1);        // start conversion, with parasite power on at the end
-  
-  delay(1000);     // maybe 750ms is enough, maybe not
-  // we might do a ds.depower() here, but the reset will take care of it.
-  
-  present = ds.reset();
-  ds.select(addr);    
-  ds.write(0xBE);         // Read Scratchpad
-
-  //Serial.print("  Data = ");
- // Serial.print(present, HEX);
-  //Serial.print(" ");
-  for ( i = 0; i < 9; i++) {           // we need 9 bytes
-    data[i] = ds.read();
-    //Serial.print(data[i], HEX);
-    //Serial.print(" ");
-  }
-  //Serial.print(" CRC=");
-  //Serial.print(OneWire::crc8(data, 8), HEX);
-  //Serial.println();
-
-  // Convert the data to actual temperature
-  // because the result is a 16 bit signed integer, it should
-  // be stored to an "int16_t" type, which is always 16 bits
-  // even when compiled on a 32 bit processor.
-  int16_t raw = (data[1] << 8) | data[0];
-  if (type_s) {
-    raw = raw << 3; // 9 bit resolution default
-    if (data[7] == 0x10) {
-      // "count remain" gives full 12 bit resolution
-      raw = (raw & 0xFFF0) + 12 - data[6];
-    }
-  } else {
-    byte cfg = (data[4] & 0x60);
-    // at lower res, the low bits are undefined, so let's zero them
-    if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
-    else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
-    else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
-    //// default is 12 bit resolution, 750 ms conversion time
-  }
-  celsius = (float)raw / 16.0;
-  fahrenheit = celsius * 1.8 + 32.0;
-
-  return fahrenheit;
+    return sensors.getTempF(outsideThermometer);
+  //return sensors.getTempFByIndex(0);
 //return 1;
   //return steinhart;
 }
